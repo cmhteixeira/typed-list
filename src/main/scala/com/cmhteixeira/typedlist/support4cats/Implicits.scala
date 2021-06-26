052264f710fe30860cfd6145b41592efbcf63beb
Carlos Teixeira <c.mh.teixeira@gmail.com>
2021-06-26 22:29:50 +0100

    Adding kind-project compiler plugin

--- src/main/scala/com/cmhteixeira/typedlist/support4cats/Implicits.scala
+++ src/main/scala/com/cmhteixeira/typedlist/support4cats/Implicits.scala
@@ -7,8 +7,8 @@ import com.cmhteixeira.typedlist.{TypedCons, TypedList, TypedNil}
 
 trait Implicits {
 
-  implicit def catsTraverseInstance[Size <: Natural]: Traverse[({ type alpha[A] = TypedList[Size, A] })#alpha] =
-    new Traverse[({ type alpha[A] = TypedList[Size, A] })#alpha] {
+  implicit def catsTraverseInstance[Size <: Natural]: Traverse[TypedList[Size, *]] =
+    new Traverse[TypedList[Size, *]] {
 
       override def traverse[G[_], A, B](fa: TypedList[Size, A])(f: A => G[B])(
           implicit evidence$1: Applicative[G]
@@ -21,19 +21,19 @@ trait Implicits {
       }
 
       override def foldLeft[A, B](fa: TypedList[Size, A], b: B)(f: (B, A) => B): B = {
-        val myApplicative: Applicative[({ type alpha[A] = Function[B, B] })#alpha] =
-          new Applicative[({ type alpha[A] = Function[B, B] })#alpha] {
+        val myApplicative: Applicative[Lambda[A => Function[B, B]]] =
+          new Applicative[Lambda[A => Function[B, B]]] {
             override def pure[K](x: K): Function[B, B] = identity
 
             override def ap[K, L](ff: Function[B, B])(fa: Function[B, B]): Function[B, B] = b => fa(ff(b))
           }
 
-        traverse[({ type alpha[A] = Function[B, B] })#alpha, A, B](fa)((a: A) => (b: B) => f(b, a))(myApplicative)(b)
+        traverse[Lambda[A => Function[B, B]], A, B](fa)((a: A) => (b: B) => f(b, a))(myApplicative)(b)
       }
 
       override def foldRight[A, B](fa: TypedList[Size, A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] = {
-        val myApplicative: Applicative[({ type alpha[A] = Function[Eval[B], Eval[B]] })#alpha] =
-          new Applicative[({ type alpha[A] = Function[Eval[B], Eval[B]] })#alpha] {
+        val myApplicative: Applicative[Lambda[A => Function[Eval[B], Eval[B]]]] =
+          new Applicative[Lambda[A => Function[Eval[B], Eval[B]]]] {
             override def pure[K](x: K): Function[Eval[B], Eval[B]] = identity
 
             override def ap[K, L](ff: Function[Eval[B], Eval[B]])(
@@ -41,7 +41,7 @@ trait Implicits {
             ): Function[Eval[B], Eval[B]] = b => ff(fa(b))
           }
 
-        traverse[({ type alpha[A] = Function[Eval[B], Eval[B]] })#alpha, A, B](fa) { (a: A) => (b: Eval[B]) =>
+        traverse[Lambda[A => Function[Eval[B], Eval[B]]], A, B](fa) { (a: A) => (b: Eval[B]) =>
           f(a, b)
         }(myApplicative)(lb)
       }
